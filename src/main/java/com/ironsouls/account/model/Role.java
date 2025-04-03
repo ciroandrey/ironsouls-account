34e3e151866399fc62f622598f744ade2bb7be67
Ciro A <ciroandrey@outlook.com>
2025-04-03 17:41:35 -0300

    Adjusting h2 console with seed data

--- src/main/java/com/ironsouls/account/model/Role.java
+++ src/main/java/com/ironsouls/account/model/Role.java
@@ -3,12 +3,13 @@ package com.ironsouls.account.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -18,8 +19,8 @@ import lombok.Setter;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     @Column(nullable = false, unique = true)
     private String name;
