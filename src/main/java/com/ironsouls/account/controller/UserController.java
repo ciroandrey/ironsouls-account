05b5b118965d71f8eb666b493cf83b5c03d47918
Ciro A <ciroandrey@outlook.com>
2025-06-18 19:15:53 -0300

    setting methods to save user and get users

--- src/main/java/com/ironsouls/account/controller/UserController.java
+++ src/main/java/com/ironsouls/account/controller/UserController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -18,11 +21,23 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable UUID id) {
         return userService.getUserById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User userPersisted = userService.saveUser(user);
+        return ResponseEntity.ok(userPersisted);
     }
 
 }
--- src/main/java/com/ironsouls/account/service/UserService.java
+++ src/main/java/com/ironsouls/account/service/UserService.java
@@ -5,6 +5,7 @@ import com.ironsouls.account.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,8 +15,16 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
     public Optional<User> getUserById(UUID userId){
         return userRepository.findById(userId);
+    }
+
+    public User saveUser (User user) {
+        return userRepository.save(user);
     }
 
 }
